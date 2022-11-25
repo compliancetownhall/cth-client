6fe90a692bb1b8d1c43923d3e7cbd6a1862e34bf
compliancetownhall <cthdotcom@gmail.com>
2022-11-26 01:50:40 +0530

    frontend changes

---
+++ src/components/Features/Features.jsx
@@ -0,0 +1,93 @@
+import {
+    Container,
+    SimpleGrid,
+    Image,
+    Flex,
+    Heading,
+    Text,
+    Stack,
+    StackDivider,
+    Icon,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+
+  
+  const Feature = ({ text, icon, iconBg }) => {
+    return (
+      <Stack direction={'row'} align={'center'}>
+        <Flex
+          w={8}
+          h={8}
+          align={'center'}
+          justify={'center'}
+          rounded={'full'}
+          bg={iconBg}>
+          {icon}
+        </Flex>
+        <Text fontWeight={600}>{text}</Text>
+      </Stack>
+    );
+  };
+  
+  export default function Features() {
+    return (
+      <Container maxW={'5xl'} py={12}>
+        <SimpleGrid columns={{ base: 1, md: 2 }} spacing={10}>
+          <Stack spacing={4}>
+            <Text
+              textTransform={'uppercase'}
+              color={'blue.400'}
+              fontWeight={600}
+              fontSize={'sm'}
+              bg={useColorModeValue('blue.50', 'blue.900')}
+              p={2}
+              alignSelf={'flex-start'}
+              rounded={'md'}>
+              What we Provide
+            </Text>
+            <Heading> Check it out </Heading>
+            <Text color={'gray.500'} fontSize={'lg'}>
+              this will be helping text describing about the compliance townhall
+            </Text>
+            <Stack
+              spacing={4}
+              divider={
+                <StackDivider
+                  borderColor={useColorModeValue('gray.100', 'gray.700')}
+                />
+              }>
+              <Feature
+                icon={
+                  <Icon  color={'yellow.500'} w={5} h={5} />
+                }
+                iconBg={useColorModeValue('yellow.100', 'yellow.900')}
+                text={'Open Discussions'}
+              />
+              <Feature
+                icon={<Icon  color={'green.500'} w={5} h={5} />}
+                iconBg={useColorModeValue('green.100', 'green.900')}
+                text={'One To One Support'}
+              />
+              <Feature
+                icon={
+                  <Icon  color={'purple.500'} w={5} h={5} />
+                }
+                iconBg={useColorModeValue('purple.100', 'purple.900')}
+                text={'Need help in your CSR'}
+              />
+            </Stack>
+          </Stack>
+          <Flex>
+            <Image
+              rounded={'md'}
+              alt={'feature image'}
+              src={
+                'https://images.unsplash.com/photo-1554200876-56c2f25224fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80'
+              }
+              objectFit={'cover'}
+            />
+          </Flex>
+        </SimpleGrid>
+      </Container>
+    );
+  }
